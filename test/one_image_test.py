38faa354b2f70632768fba66481a49db992819c1
Dove Zachary <504001583@qq.com>
2019-05-24 18:56:52 +0800

    Update one_image_test.py

--- test/one_image_test.py
+++ test/one_image_test.py
@@ -17,7 +17,6 @@ slide_window = False
 shuffle = False
 detectors = [None, None, None]
 prefix = ['data/ultramodern_model/PNet', 'data/ultramodern_model/RNet', 'data/ultramodern_model/ONet']
-epoch = [1, 22, 22]
 batch_size = [2048, 64, 16]
 # model_path = ['%s-%s' % (x, y) for x, y in zip(prefix, epoch)]
 # load pnet model

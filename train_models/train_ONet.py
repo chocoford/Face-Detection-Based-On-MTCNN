11e9688aeb3f3210d03da8ac67c07c11a13f66c6
Dove Zachary <504001583@qq.com>
2019-05-28 21:16:30 +0800

    Update train_ONet.py

--- train_models/train_ONet.py
+++ train_models/train_ONet.py
@@ -1,21 +1,18 @@
 #coding:utf-8
 from train_models.mtcnn_model import O_Net, cls_ohem, bbox_ohem, landmark_ohem, cal_accuracy
-from train_models.utils import get_dataset
+from train_models.utils import get_dataset, load_and_get_normalization_img
 import tensorflow as tf
 import time, os, sys
 tf.enable_eager_execution()
 
-def cls_loss(model, x, label):
-    cls_prob = model(x)[0]
-    return cls_ohem(cls_prob,label)
+def cls_loss(pred, label):
+    return cls_ohem(pred, label)
 
-def bbox_loss(model, x, bbox_target, label):
-    bbox_pred = model(x)[1]
-    return bbox_ohem(bbox_pred,bbox_target,label)
+def bbox_loss(pred, bbox_target, label):
+    return bbox_ohem(pred, bbox_target, label)
 
-def landmark_loss(model, x,landmark_target, label):
-    landmark_pred = model(x)[2]
-    return landmark_ohem(landmark_pred, landmark_target, label)
+def landmark_loss(pred, landmark_target, label):
+    return landmark_ohem(pred, landmark_target, label)
 
 
 def cls_acc(cls_pred, labels):
@@ -23,15 +20,17 @@ def cls_acc(cls_pred, labels):
 
 
 def loss(model, images, labels, bboxes, landmarks):
-    c_loss = cls_loss(model, images, labels)
-    b_loss = bbox_loss(model, images, bboxes, labels)
-    l_loss = landmark_loss(model, images, landmarks, labels)
-    return c_loss + 0.5 * b_loss + l_loss
+    pred = model(images)
+    c_loss = cls_loss(pred[0], labels)
+    b_loss = bbox_loss(pred[1], bboxes, labels)
+    l_loss = landmark_loss(pred[2], landmarks, labels)
+    return c_loss * 0.5 + 0.5 * b_loss + l_loss
 
 def grad(model, images, labels, bboxes, landmarks):
     with tf.GradientTape() as tape:
         loss_value = loss(model, images, labels, bboxes, landmarks)
     return loss_value, tape.gradient(loss_value, model.trainable_variables)
+
 
 def train_ONet(base_dir, checkpoint_dir, end_epoch, display_step, lr):
     """
@@ -46,16 +45,19 @@ def train_ONet(base_dir, checkpoint_dir, end_epoch, display_step, lr):
     model = O_Net()
     batch_size = 256
     total_num, train_dataset = get_dataset(base_dir, batch_size=batch_size)
-    optimizer = tf.train.MomentumOptimizer(lr, 0.9)
+    optimizer = tf.train.AdamOptimizer()
 
 
     # prepare for save
     os.makedirs(checkpoint_dir, exist_ok=True)
     checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
     root = tf.train.Checkpoint(optimizer=optimizer, model=model)
+    # if continue training process
+    # root.restore(tf.train.latest_checkpoint(prefix)).assert_existing_objects_matched()
+
 
     now = time.time()
-    pred = now
+    pre = now
 
     print("start training")
     for epoch in range(end_epoch):
@@ -68,24 +70,33 @@ def train_ONet(base_dir, checkpoint_dir, end_epoch, display_step, lr):
             loss_value, grads = grad(model, images, labels, bboxes, landmarks)
             optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
-            acc_value = cls_acc(model(images)[0], labels)
-
+            display_pred = model(images)
+            acc_value = cls_acc(display_pred[0], labels)
+            c_loss = cls_loss(display_pred[0], labels)
+            b_loss = bbox_loss(display_pred[1], bboxes, labels)
+            l_loss = landmark_loss(display_pred[2], landmarks, labels)
             epoch_loss_avg(loss_value)
             epoch_accuracy_avg(acc_value)
 
             if i % display_step == 0:
                 now = time.time()
                 total_steps = total_num // batch_size
-                remaining_time = (now - pred) * (total_steps - i) / display_step // 60
-                sys.stdout.write("\r>> {0} of {1} steps done. Estimated remaining time: {2} mins. loss_value: {3} acc: {4}".format(i, 
-                                                                                                                                   total_steps, 
-                                                                                                                                   remaining_time,
-                                                                                                                                   loss_value.numpy(),
-                                                                                                                                   acc_value.numpy()))
+                remaining_time = (now - pre) * (total_steps - i) / display_step // 60
+                sys.stdout.write("\r>> {0} of {1} steps done. Estimated remaining time: {2} mins. \
+loss_value: {3:.3f} acc: {4:.3f}. cls_loss: {5:.3f}, bbox_loss: {6:.3f}, landmark_loss: {7:.3f}".format(i, 
+                                                                                                        total_steps, 
+                                                                                                        remaining_time,
+                                                                                                        loss_value.numpy(),
+                                                                                                        acc_value.numpy(),
+                                                                                                        c_loss.numpy(),
+                                                                                                        b_loss.numpy(),
+                                                                                                        l_loss.numpy()))
                 sys.stdout.flush()  
-                pred = now
+                pre = now
 
-        print("\rEpoch {0}: Loss: {1} Accuracy: {2} ".format(epoch, epoch_loss_avg.result(), epoch_accuracy_avg.result()))
+        print("\nEpoch {0}: Loss: {1} Accuracy: {2}".format(epoch, epoch_loss_avg.result(), epoch_accuracy_avg.result()))
+        print("VALIDATION: try to predict a pos pic for cls_prob: ", model(tf.expand_dims(load_and_get_normalization_img("test/not test/7.jpg", 48), axis=0))[0])
+        print("VALIDATION: try to predict a neg pic for cls_prob: ", model(tf.expand_dims(load_and_get_normalization_img("test/not test/10001.jpg", 48), axis=0))[0])
 
         # save model
         save_path = root.save(checkpoint_prefix)
@@ -99,7 +110,7 @@ if __name__ == '__main__':
 
     base_dir = '../DATA/imglists/ONet'
     model_name = 'ultramodern'
-    model_path = '../data/%s_model/ONet_landmark/ONet' % model_name
+    model_path = '../data/%s_model/ONet' % model_name
     prefix = model_path
     end_epoch = 22
     display = 10
